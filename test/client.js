f666d52876f5c8f79afea7f08be486c2b486c198
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-15 10:26:35 +0100

    Add tests

---
+++ test/client.js
@@ -0,0 +1,30 @@
+const expect = require("chai").expect
+const LoginClient = require("../lib/login-client")
+let client = new LoginClient("login.example.com")
+
+describe("Connect", () => {
+
+  /**
+   * Will be removed when connect() is implemented.
+   */
+  it("should throw an error if connect is called", done => {
+    expect(() => client.connect()).to.throw
+    done()
+  })
+
+  it("should throw an error if an unknown event listener is added", done => {
+    expect(() => client.addEventListener("abcdefg", () => {})).to.throw
+    done()
+  })
+
+  it("should contain default values in getter properties", done => {
+    expect(client.isLoggedIn).to.be.false
+    expect(client.providers).to.be.empty
+    expect(client.user).to.be.null
+    expect(client.connected).to.be.false
+    expect(client.token).to.be.null
+    expect(client.publicKey).to.be.null
+    done()
+  })
+
+})
