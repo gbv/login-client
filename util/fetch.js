c51e41679bb8e75d913f48cd5e30725699c104bd
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-15 11:10:40 +0100

    Add a node-fetch wrapper to utils

---
+++ util/fetch.js
@@ -0,0 +1,22 @@
+/**
+ * A wrapper around node-fetch that throws an error if the request was not okay.
+ */
+
+const fetch = require("node-fetch")
+
+class FetchError extends Error {
+  constructor(message, status) {
+    super(message)
+    this.status = status
+  }
+}
+
+module.exports = (url, options) => {
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res
+    } else {
+      throw new FetchError(res.statusText, res.status)
+    }
+  })
+}
